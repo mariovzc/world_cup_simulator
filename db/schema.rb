6cc0ac8b12df81745893884ab3bd46180bce4470
Mario vizcaino <mario.vizcainos@gmail.com>
2018-01-23 11:23:37 -0500

    added group models

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180123161216) do
+ActiveRecord::Schema.define(version: 20180123162154) do
+
+  create_table "groups", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "stadia", force: :cascade do |t|
     t.string "name"
