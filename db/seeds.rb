00662e86027697b4bd9b7330de56534d1028d51f
Mario vizcaino <mario.vizcainos@gmail.com>
2018-01-23 17:59:00 -0500

    added seeds for game types, sub types, stadiums and teams

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,32 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+p "Init seed"
+
+p "Groups and sub groups Seed"
+
+game_types = ["groups", "knockout"]
+
+sub_groups = [('A'..'H').to_a, ["Round of 16", "Quarter-finals", "Semi-finals", "Third place play-off", "Final"]]
+
+game_types.each_with_index do |type, index|
+  t = GameType.first_or_create(name: type)
+  sub_groups[index].each do |sub|
+    GameSubType.first_or_create(name: sub, game_type: t)
+  end
+end
+
+p "Finish group and sub groups seed"
+
+json = ActiveSupport::JSON.decode(File.read("db/data.json"))
+
+p "Stadiums seed"
+json["stadiums"].each do |stadium|
+  Stadium.first_or_create(name: stadium["name"], city: stadium["city"])
+end
+p "Finish stadium seed"
+
+p "Teams Seed"
+json["teams"].each do |team|
+  Team.first_or_create(name: team["name"], iso2: team["iso2"])
+end
+p "Finish team Seed"
+
+p "Seed finished"
