27e8cbbeab7c3d4c1a08fef6da44bd91f3de952b
Mario vizcaino <mario.vizcainos@gmail.com>
2018-01-24 21:05:24 -0500

    added function to create and delete the world cup

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,2 +1,43 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
+$(document).on('turbolinks:load', function () {
+  document.getElementById('reset').addEventListener('click', function () {
+    sweetAlert({
+      title: '',
+      text: 'Are you sure you want to restart the info',
+      type: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'Yes, delete it!',
+      closeOnConfirm: false
+    },
+    function () {
+      axios({
+        method: 'DELETE',
+        url: '/api/v1/restart'
+      })
+      .then(function(response) {
+        sweetAlert('', 'The data has been reset!.', 'success')
+        document.location.reload()
+      })
+      .catch(function (error) {
+        sweetAlert('', 'Please try again', 'danger')
+      })
+    })
+  })
+  document.getElementById('start').addEventListener('click', function () {
+    axios({
+      method: 'POST',
+      url: '/api/v1/create_matches'
+    }).then(function (response) {
+      sweetAlert({
+        title: '',
+        text: 'The world cup has ben created!!!',
+        type: 'warning',
+        showCancelButton: false,
+        confirmButtonText: 'Great!',
+        closeOnConfirm: false
+      },
+      function () {
+        document.location.reload()
+      })
+    })
+  })
+})
