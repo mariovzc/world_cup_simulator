735f5929e6b51f5059963a8c7eadd15d315c2888
Mario vizcaino <mario.vizcainos@gmail.com>
2018-01-24 21:32:44 -0500

    added some validations when you create the matches

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,43 +1,51 @@
 $(document).on('turbolinks:load', function () {
-  document.getElementById('reset').addEventListener('click', function () {
-    sweetAlert({
-      title: '',
-      text: 'Are you sure you want to restart the info',
-      type: 'warning',
-      showCancelButton: true,
-      confirmButtonText: 'Yes, delete it!',
-      closeOnConfirm: false
-    },
-    function () {
-      axios({
-        method: 'DELETE',
-        url: '/api/v1/restart'
-      })
-      .then(function(response) {
-        sweetAlert('', 'The data has been reset!.', 'success')
-        document.location.reload()
-      })
-      .catch(function (error) {
-        sweetAlert('', 'Please try again', 'danger')
-      })
-    })
-  })
-  document.getElementById('start').addEventListener('click', function () {
-    axios({
-      method: 'POST',
-      url: '/api/v1/create_matches'
-    }).then(function (response) {
+
+  var reset = document.getElementById('reset')
+  if (document.body.contains(reset)) {
+    reset.addEventListener('click', function () {
       sweetAlert({
         title: '',
-        text: 'The world cup has ben created!!!',
+        text: 'Are you sure you want to restart the info',
         type: 'warning',
-        showCancelButton: false,
-        confirmButtonText: 'Great!',
+        showCancelButton: true,
+        confirmButtonText: 'Yes, delete it!',
         closeOnConfirm: false
       },
       function () {
-        document.location.reload()
+        axios({
+          method: 'DELETE',
+          url: '/api/v1/restart'
+        })
+        .then(function(response) {
+          sweetAlert('', 'The data has been reset!.', 'success')
+          document.location.reload()
+        })
+        .catch(function (error) {
+          sweetAlert('', 'Please try again', 'danger')
+        })
       })
     })
-  })
+  }
+  var start = document.getElementById('start')
+  if (document.body.contains(reset)) {
+    start.addEventListener('click', function () {
+      start.parentElement.style.display = 'none'
+      axios({
+        method: 'POST',
+        url: '/api/v1/create_matches'
+      }).then(function (response) {
+        sweetAlert({
+          title: '',
+          text: 'The world cup has ben created!!!',
+          type: 'warning',
+          showCancelButton: false,
+          confirmButtonText: 'Great!',
+          closeOnConfirm: false
+        },
+        function () {
+          document.location.reload()
+        })
+      })
+    })
+  }
 })
